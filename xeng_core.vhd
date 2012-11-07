43b5e0d1b0dddf10ce89e81060c6700f0b1e61ed
Rurik A Primiani <rprimian@cfa.harvard.edu>
2012-11-07 10:03:16 -0500

    Migrated to the standard CASPER X-engine.
    We're using an integration time of 128 which means that this block
    will need to be fed 128 samples per channel per antenna. This will be
    accomplished with a QDR-based matrix transpose in the sma_corr design
    (this change is forthcoming).
    
    Compile using sma-wideband/mlib_devel@e05d91d.

--- xeng_core.vhd
+++ xeng_core.vhd
@@ -3,19 +3,16 @@ use IEEE.std_logic_1164.all;
 
 entity xeng_core is
   port (
+    ant: in std_logic_vector(15 downto 0); 
     ce_1: in std_logic; 
     clk_1: in std_logic; 
-    i0_pol0: in std_logic_vector(7 downto 0); 
-    i0_pol1: in std_logic_vector(7 downto 0); 
-    i1_pol0: in std_logic_vector(7 downto 0); 
-    i1_pol1: in std_logic_vector(7 downto 0); 
-    newacc: in std_logic; 
-    bin: out std_logic_vector(10 downto 0); 
-    p0xp0: out std_logic_vector(63 downto 0); 
-    p0xp1: out std_logic_vector(63 downto 0); 
-    p1xp0: out std_logic_vector(63 downto 0); 
-    p1xp1: out std_logic_vector(63 downto 0); 
-    rdy: out std_logic
+    mcnt_in: in std_logic_vector(31 downto 0); 
+    sync_in: in std_logic; 
+    window_valid: in std_logic; 
+    burst_out: out std_logic_vector(31 downto 0); 
+    mcnt_out: out std_logic_vector(31 downto 0); 
+    sync_out: out std_logic; 
+    valid_out: out std_logic
   );
 end xeng_core;
 
