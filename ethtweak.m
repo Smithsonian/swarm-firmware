8bc8299a7b9dd1d1c64366572f3b941534b11f3d
David MacMahon <davidm@astro.berkeley.edu>
2013-04-28 19:30:19 -0400

    Fix Goto tags in network/eth/N blocks
    This was leftover from incomplete modifications after some copy/paste
    operations (copying network/eth/0 to network/eth{1..3}).  The "ethtweak"
    function in the newly added "ethtweak.m" file can be used to fix these up in
    the future.

---
+++ ethtweak.m
@@ -0,0 +1,12 @@
+function ethtweak(sys)
+  for k = 1:3
+    set_param([sys '/network/eth/' num2str(k) '/bsdata'], 'OutputSignals', sprintf('dout%d,valid%d', k, k));
+    set_param([sys '/network/eth/' num2str(k) '/bseof'], 'OutputSignals', sprintf('eof%d', k));
+    set_param([sys '/network/eth/' num2str(k) '/core'], 'port_r2_sfpp', num2str(k));
+    gotos = find_system([sys '/network/eth/' num2str(k)], 'RegExp','on', 'SearchDepth', '1', 'BlockType','Goto','GotoTag','_0$');
+    for g = 1:length(gotos)
+      tag = regexprep(get_param(gotos{g}, 'GotoTag'), '_0$', ['_', num2str(k)]);        
+      set_param(gotos{g}, 'GotoTag', tag);
+    end
+  end
+end
