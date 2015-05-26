e6c99f6852d94b1b00b5f814d53d9eb36d79d6e2
Rurik A Primiani <rprimian@cfa.harvard.edu>
2015-05-26 18:07:46 -0400

    [swarm_resynth_core.m] Added a MATLAB script for resynthing SWARM cores.

---
+++ swarm_resynth_core.m
@@ -0,0 +1,25 @@
+function swarm_resynth_core(core_name)
+% SWARM_RESYNTH_CORE Re-synthesize a particular SWARM 
+%      core with the "standard" set of XST parameters:
+%
+%      -opt_mode=area
+%      -glob_opt=allclocknets
+%      -register_balancing=yes
+%      -optimize_primitives=yes'
+%      -read_cores=yes
+%      -lc=off
+
+    xst_args = {'-opt_mode', 'area', ...
+        '-glob_opt', 'allclocknets', ...
+        '-register_balancing', 'yes', ...
+        '-optimize_primitives', 'yes', ...
+        '-read_cores', 'yes',...
+        '-lc', 'off'};
+
+    if strcmp(core_name, 'sma_corr')
+        resynth_netlist('sma_corr/sysgen', 'sma_corr_cw', xst_args{:});
+    else
+        resynth_netlist(core_name, xst_args{:});
+    end
+end
+
