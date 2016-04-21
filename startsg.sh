729b80a5783a5066b3163a95c7c42c91a88aaee5
Rurik A Primiani <rprimian@cfa.harvard.edu>
2016-04-21 14:13:09 -0400

    [startsg.sh] Pass command line arguments to MATLAB (through sysgen -a).
    This means you can run startsg using any MATLAB arguments, particularly
    useful are "-nosplash" and "-nodesktop".

--- startsg.sh
+++ startsg.sh
@@ -6,6 +6,9 @@ export XILINX_PATH=/opt/Xilinx/14.7/ISE_DS
 export MLIB_DEVEL_PATH=../mlib_devel
 export MLIB_ROOT=$MLIB_DEVEL_PATH
 export MATLAB=$MATLAB_PATH
+####### Annoying stuff due to Xilinx taking args ######
+SGARGS=\""$@"\"
+shift $# # wipe all args
 source $XILINX_PATH/settings64.sh
 ####### These are some SWARM-specific settings ######
 export USER_UCF_PATH='./sma_corr/pblocks' # SWARM area groups
@@ -13,4 +16,4 @@ export QDR_MMCM_LOCK_EN=yes # Enable MMCM lock feature for QDR
 export IDCTRL_100=yes # User 100 MHz for IDELAY_CTRL
 ####### Add MATLAB to path and start SysGen ######
 export PATH=$MATLAB/bin:$PATH
-sysgen
+sysgen -a "$SGARGS"
